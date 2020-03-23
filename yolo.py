c6daf03ab22e8865337d430eab919d74285e93ba
Navaz <navazp30@gmail.com>
2020-03-23 23:47:13 +0530

    added audio

--- yolo.py
+++ yolo.py
@@ -8,11 +8,19 @@ import subprocess
 from gtts import gTTS
 
 
+#speakgtts
+def speak_from_string_text(speaktext):
+	if speaktext:
+		audio = gTTS(text= speaktext,lang = 'en',slow = False)
+		audio.save("output.mp3")
+		#subprocess.call(["C:\Program Files\VideoLAN\VLC\vlc.exe","-nodisp","-autoexit","output.mp3"])
+		os.system('output.mp3 &')
+	
 #construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
+ap.add_argument("-i", "--image", required=False,
 	help="path to input image")
-ap.add_argument("-y", "--yolo", required=True,
+ap.add_argument("-y", "--yolo", required=False,
 	help="base path to YOLO directory")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
@@ -21,7 +29,7 @@ ap.add_argument("-t", "--threshold", type=float, default=0.3,
 args = vars(ap.parse_args())
 
 # load the COCO class labels our YOLO model was trained on
-labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+labelsPath = os.path.sep.join(["yolo-coco", "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
 # initialize a list of colors to represent each possible class label
@@ -30,8 +38,8 @@ COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
 	dtype="uint8")
 
 # derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
-configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+weightsPath = os.path.sep.join(["yolo-coco", "yolov3.weights"])
+configPath = os.path.sep.join(["yolo-coco", "yolov3.cfg"])
 
 # load our YOLO object detector trained on COCO dataset (80 classes)
 print("[INFO] loading YOLO from disk...")
@@ -71,6 +79,7 @@ print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 boxes = []
 confidences = []
 classIDs = []
+centers = []
 
 
 
@@ -101,6 +110,7 @@ for output in layerOutputs:
 			boxes.append([x, y, int(width), int(height)])
 			confidences.append(float(confidence))
 			classIDs.append(classID)
+			centers.append((centerX,centerY))
 
 # apply non-maxima suppression to suppress weak, overlapping bounding
 # boxes
@@ -108,37 +118,43 @@ idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
 	args["threshold"])
 
 # ensure at least one detection exists
-texts = []
+texts = ''
 if len(idxs) > 0:
 	# loop over the indexes we are keeping
 	for i in idxs.flatten():
 		# extract the bounding box coordinates
 		(x, y) = (boxes[i][0], boxes[i][1])
 		(w, h) = (boxes[i][2], boxes[i][3])
+		
 		# draw a bounding box rectangle and label on the image
 		color = [int(c) for c in COLORS[classIDs[i]]]
 		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-		if x <= w:
+		
+		#extract center
+		centerX, centerY = centers[i][0], centers[i][1]
+		
+		
+		if centerX <= W/3:
 			W_pos = "left "
-		elif x <= (w/3 * 2):
+		elif centerX <= (W/3 * 2):
 			W_pos = "center "
 		else:
 			W_pos = "right "			
-		if y <= h/3:
+		if centerY <= H/3:
 			H_pos = "top "
-		elif y <= (h/3 * 2):
+		elif centerY <= (H/3 * 2):
 			H_pos = "mid "
 		else:
 			H_pos = "bottom "
+			
+		print("x : {},y : {},W: {},H: {},w: {},h: {}".format(x,y,W,H,w,h))
 		# text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
 		#text = "{}: {}".format(LABELS[classIDs[i]], "found")
-		texts.append(H_pos + W_pos + LABELS[classIDs[i]])
+		texts = texts + H_pos + W_pos + LABELS[classIDs[i]] + " "
 		#cv2.putText(image, text[i], (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
 		#	0.5, color, 2)
 print(texts)
-#audio = gTTS(text= text,lang = 'en')
-#audio.save("output.mp3")
-#subprocess.call(["vlc","-nodisp","-autoexit","output.mp3"])
+speak_from_string_text(texts)
 		
 
 # show the output image
